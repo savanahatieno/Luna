39b89a109251280c84f7df87479de7c75cefa76e
savanahatieno <savanaht01@gmail.com>
2021-05-15 21:04:12 +0300

    removed the action bar and replaced it toolbar

--- app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
+++ app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
@@ -3,7 +3,10 @@ package com.moringaschool.luna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 import android.view.WindowManager;
+
+import java.util.Objects;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -13,8 +16,9 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         setContentView(R.layout.activity_welcome_screen);
     }
