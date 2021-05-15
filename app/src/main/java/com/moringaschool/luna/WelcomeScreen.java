a02fab39536f03d0dd672d5d78a5dbbdc7a51dc7
savanahatieno <savanaht01@gmail.com>
2021-05-15 20:10:35 +0300

    set the screen timeout

--- app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
+++ app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    private static final int WELCOME_SCREEN_TIMEOUT = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
