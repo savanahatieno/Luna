6d26414b9c2da04d93b9bc253947518b7206c2f9
savanahatieno <savanaht01@gmail.com>
2021-05-15 20:12:53 +0300

    enabled full screen to welcome page

--- app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
+++ app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
@@ -3,6 +3,7 @@ package com.moringaschool.luna;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -11,6 +12,10 @@ public class WelcomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_welcome_screen);
     }
 }
