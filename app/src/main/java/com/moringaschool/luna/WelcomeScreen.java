36cf51bd69de991686c7c0f28eb9a21fbe52d626
savanahatieno <savanaht01@gmail.com>
2021-05-15 22:10:26 +0300

    setting timer for the welcome screen

--- app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
+++ app/src/main/java/com/moringaschool/luna/WelcomeScreen.java
@@ -2,7 +2,9 @@ package com.moringaschool.luna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -10,16 +12,13 @@ import java.util.Objects;
 
 public class WelcomeScreen extends AppCompatActivity {
 
-    private static final int WELCOME_SCREEN_TIMEOUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_welcome_screen);
+
+
+
     }
 }
