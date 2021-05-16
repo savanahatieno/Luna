263d6cd7669692f8aa41033aa632ba79d03e3138
savanahatieno <savanaht01@gmail.com>
2021-05-16 21:12:27 +0300

    added a viewonclick listener

--- app/src/main/java/com/moringaschool/luna/Login.java
+++ app/src/main/java/com/moringaschool/luna/Login.java
@@ -2,7 +2,9 @@ package com.moringaschool.luna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class Login extends AppCompatActivity {
@@ -12,5 +14,16 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        View callSignUp = findViewById(R.id.signup_screen);
+        
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Signup.class);
+                startActivity(intent);
+            }
+        });
+        
     }
 }
