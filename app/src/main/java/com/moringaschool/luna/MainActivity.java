5be22049782e1c89de7aa3bf1bb3f8af72b515e6
savanahatieno <savanaht01@gmail.com>
2021-05-16 17:08:18 +0300

    setting up animation duration

--- app/src/main/java/com/moringaschool/luna/MainActivity.java
+++ app/src/main/java/com/moringaschool/luna/MainActivity.java
@@ -4,15 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView imageView;
+    TextView textView1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //HOOKS
+        imageView = findViewById(R.id.imageview);
+        textView1 = findViewById(R.id.textView1);
+
+
+        imageView.setAnimation(topAnim);
+        textView1.setAnimation(bottomAnim);
+
+
+
 
     }
 
