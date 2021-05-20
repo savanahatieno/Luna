174bd206af08cea7f1617a61c3a2a7ef6cff65c1
savanahatieno <savanaht01@gmail.com>
2021-05-20 14:49:41 +0300

    added validation to login form

--- app/src/main/java/com/moringaschool/luna/Login.java
+++ app/src/main/java/com/moringaschool/luna/Login.java
@@ -8,16 +8,54 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
 public class Login extends AppCompatActivity {
+
+    EditText eFullName, eEmail, ePassword;
+    AwesomeValidation awesomeValidation;
+
+
     private Button loginButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        eFullName = findViewById(R.id.fullname);
+        eEmail = findViewById(R.id.emailaddress);
+        ePassword = findViewById(R.id.password);
+
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        //Add Validation
+
+        awesomeValidation.addValidation(this,R.id.fullname, RegexTemplate.NOT_EMPTY,R.string.invalid_name);
+        awesomeValidation.addValidation(this,R.id.emailaddress, RegexTemplate.NOT_EMPTY,R.string.invalid_emailaddress);
+        awesomeValidation.addValidation(this,R.id.password, RegexTemplate.NOT_EMPTY,R.string.invalid_password);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (awesomeValidation.validate()){
+                    Toast.makeText(getApplicationContext()
+                    ,"Form Validate Successfully...",Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext()
+                    ,"Validation Failed....",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
         loginButton = (Button) findViewById(R.id.loginButton);
 
