47cb5b9ea6c61731edc52749dbf646a5bcc4c4f3
savanahatieno <savanaht01@gmail.com>
2021-05-20 11:34:14 +0300

    added onclick listener

--- app/src/main/java/com/moringaschool/luna/Signup.java
+++ app/src/main/java/com/moringaschool/luna/Signup.java
@@ -2,15 +2,31 @@ package com.moringaschool.luna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
 
-
+    private Button alreadybutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        alreadybutton = (Button) findViewById(R.id.alreadybuton);
+        alreadybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+    }
+    public void openLogin (){
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
     }
 }
