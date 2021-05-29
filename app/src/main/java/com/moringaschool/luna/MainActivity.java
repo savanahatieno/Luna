ec83c7228f544689f0c61a7b0cf72bb21096b9b3
savanahatieno <savanaht01@gmail.com>
2021-05-29 11:44:13 +0300

    added firebase json file

--- app/src/main/java/com/moringaschool/luna/MainActivity.java
+++ app/src/main/java/com/moringaschool/luna/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         final Handler handler = new Handler();
             handler.postDelayed(() -> {
-                startActivity(new Intent(MainActivity.this,Signup.class));
+                startActivity(new Intent(MainActivity.this,StartPage.class));
                 finish();
             },4000);
 
