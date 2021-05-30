09e002374c27a14ebaab30f62e2c7200a5dd989a
savanahatieno <savanaht01@gmail.com>
2021-05-30 20:55:32 +0300

    added todo page and created a database

---
+++ app/src/main/java/com/moringaschool/luna/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.moringaschool.luna;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ app/src/main/java/com/moringaschool/luna/TodoMain.java
@@ -0,0 +1,70 @@
+package com.moringaschool.luna;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.moringaschool.luna.Adapters.ToDoAdapter;
+import com.moringaschool.luna.Model.ToDoModel;
+import com.moringaschool.luna.Utilis.DatabaseHandler;
+
+import java.util.Collections;
+import java.util.List;
+
+public class TodoMain extends AppCompatActivity implements DialogCloseListener {
+
+    private DatabaseHandler db;
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+    private FloatingActionButton fab ;
+
+    private List <ToDoModel> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_main);
+
+
+
+        db = new DatabaseHandler(this);
+        db.openDatabase();
+
+        tasksRecyclerView = findViewById(R.id.tasksRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(db,TodoMain.this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerItemTouchHelper(tasksAdapter));
+        itemTouchHelper.attachToRecyclerView(tasksRecyclerView);
+
+        fab = findViewById(R.id.fab);
+
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+
+
+        tasksAdapter.setTasks(taskList);
+
+
+        fab.setOnClickListener(v -> {
+            AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+
+        });
+
+    }
+    @Override
+    public void handleDialogClose(DialogInterface dialog){
+        taskList = db.getAllTasks();
+        Collections.reverse(taskList);
+        tasksAdapter.setTasks(taskList);
+        tasksAdapter.notifyDataSetChanged();
+    }
+
+}
