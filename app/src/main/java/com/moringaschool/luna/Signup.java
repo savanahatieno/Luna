2b66a56bfa7567049d3c7b90ec4e222afda2a560
savanahatieno <savanaht01@gmail.com>
2021-06-01 13:40:03 +0300

    authentication sing up fix

--- app/src/main/java/com/moringaschool/luna/Signup.java
+++ app/src/main/java/com/moringaschool/luna/Signup.java
@@ -1,40 +1,29 @@
 package com.moringaschool.luna;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ProgressDialog;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-import android.text.TextUtils;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.basgeekball.awesomevalidation.AwesomeValidation;
-import com.basgeekball.awesomevalidation.ValidationStyle;
-import com.basgeekball.awesomevalidation.utility.RegexTemplate;
-import com.google.android.gms.dynamic.IFragmentWrapper;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class Signup extends AppCompatActivity implements View.OnClickListener{
+
+public class Signup extends AppCompatActivity implements View.OnClickListener {
 
 
     private FirebaseAuth mAuth;
@@ -125,16 +114,17 @@ public class Signup extends AppCompatActivity implements View.OnClickListener{
 
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View v) {
-    switch (v.getId()){
-        case R.id.banner:
-            startActivity(new Intent(this,MainActivity.class));
-            break;
-        case R.id.signupButton:
-            signupbutton();
-            break;
-    }
+        switch (v.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.signupButton:
+                signupbutton();
+                break;
+        }
     }
 
     private void signupbutton() {
@@ -143,88 +133,117 @@ public class Signup extends AppCompatActivity implements View.OnClickListener{
         String email = eEmail.getText().toString().trim();
         String password = ePassword.getText().toString().trim();
 
-        if (fullname.isEmpty()){
+        if (fullname.isEmpty()) {
             eFullName.setError("Full name is required");
             eFullName.requestFocus();
             return;
         }
-        if (username.isEmpty()){
+        if (username.isEmpty()) {
             eUsername.setError("Username is required");
             eUsername.requestFocus();
             return;
         }
-        if (email.isEmpty()){
+        if (email.isEmpty()) {
             eEmail.setError("Email is required");
             eEmail.requestFocus();
             return;
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             eEmail.setError("Please provide valid email");
             eEmail.requestFocus();
             return;
         }
-        if (password.isEmpty()){
+        if (password.isEmpty()) {
             ePassword.setError("Password is required");
             ePassword.requestFocus();
             return;
         }
-        if (password.length() < 6){
+        if (password.length() < 6) {
             ePassword.setError("Min password length should be 6 characters");
             ePassword.requestFocus();
             return;
         }
 
+
         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull  Task<AuthResult> task) {
-                      if (task.isSuccessful()){
-                          User user = new User(fullname, username, email);
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        User user = new User(fullname, username, username);
 
-                          FirebaseDatabase.getInstance().getReference("Users")
-                                  .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                  .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                              @Override
-                              public void onComplete(@NonNull Task<Void> task) {
+                        FirebaseDatabase.getInstance().getReference("users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                Toast.makeText(Signup.this, "Registered Successfully!!", Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
 
-                                  if (task.isSuccessful()){
-                                      Toast.makeText(Signup.this,"Registered Successfully!!",Toast.LENGTH_SHORT).show();
-                                      progressBar.setVisibility(View.VISIBLE);
+                                //redirect
+                            } else {
+                                Toast.makeText(Signup.this, "Failed to Register! Try Again!", Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
 
-                                      //redirect to login layout
 
-                                  }else {
-                                      Toast.makeText(Signup.this,"Failed to Register! Try Again!",Toast.LENGTH_SHORT).show();
-                                      progressBar.setVisibility(View.VISIBLE);
-                                  }
-
-                              }
-                          });
-                      }else {
-                          Toast.makeText(Signup.this,"Failed to Register",Toast.LENGTH_LONG).show();
-                          progressBar.setVisibility(View.GONE);
-                      }
-
+                            }
+                        });
+                    } else {
+                        Toast.makeText(Signup.this, "Failed to Register", Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
                     }
+
+
                 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 
 }
+
+
+//        progressBar.setVisibility(View.VISIBLE);
+//        mAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(task -> {
+//                  if (task.isSuccessful()){
+//                      User user = new User(fullname, username, email);
+//
+//                      FirebaseDatabase.getInstance().getReference("Users")
+//                              .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                              .setValue(user).addOnCompleteListener(tas -> {
+//
+//                                  if (task1.isSuccessful()){
+//                                      Toast.makeText(Signup.this,"Registered Successfully!!",Toast.LENGTH_SHORT).show();
+//                                      progressBar.setVisibility(View.GONE);
+//
+//
+//                                      //redirect to login layout
+//
+//                                  }else {
+//                                      Toast.makeText(Signup.this,"Failed to Register! Try Again!",Toast.LENGTH_SHORT).show();
+//                                      progressBar.setVisibility(View.GONE);
+//                                  }
+//
+//                              });
+//                  }else {
+//                      Toast.makeText(Signup.this,"Failed to Register",Toast.LENGTH_LONG).show();
+//                      progressBar.setVisibility(View.GONE);
+//                  }
+//
+//                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
